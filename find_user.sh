6c7f1d431426caa6c8faa320af617073414c4868
Jhonata Almeida <jhonataalmeida@MacBook-Pro-de-Jhonata.local>
2016-09-25 13:03:45 -0300

    Substituição do if pelo case, adicionado -v: versão e *:opão inválida

--- find_user.sh
+++ find_user.sh
@@ -6,16 +6,32 @@
 #
 # versão1: Mostra usuários e nomes separados por tab
 #
+# versão2: Adicionado a opção -h de ajuda
+#
+# versão3: Substituição do if pelo case, adicionado -v: versão e *:opão inválida
+#
 
 MENSAGE_USO="
-	Uso: $0 [-h]
+	Uso: $0 [-h | -v]
 	-h mostra esta tela de ajuda e sai
 "
 
-#Tratamento da linha de comando:
-if test "$1" = "-h"
-	then echo "$MENSAGE_USO"
-	exit 0
-fi
+#Adicionar case
+case "$1" in 
+	-h)
+			echo "$MENSAGE_USO"
+			exit 0
+	;;
+	
+	-v)
+			echo $0 versão 3
+			exit 0
+	;;
+	
+	*)
+			echo Opção inválida: $1	
+			exit 1
+	;;
+esac
 
 cut -d : -f 1,5 /etc/passwd | tr : \\t
