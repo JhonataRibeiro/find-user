8ee1a985c2aa2b6741b79bf75834b0e2e90c4703
Jhonata Almeida <jhonataalmeida@MacBook-Pro-de-Jhonata.local>
2016-09-25 12:54:33 -0300

    add -help option

--- find_user.sh
+++ find_user.sh
@@ -7,4 +7,15 @@
 # versão1: Mostra usuários e nomes separados por tab
 #
 
+MENSAGE_USO="
+	Uso: $0 [-h]
+	-h mostra esta tela de ajuda e sai
+"
+
+#Tratamento da linha de comando:
+if test "$1" = "-h"
+	then echo "$MENSAGE_USO"
+	exit 0
+fi
+
 cut -d : -f 1,5 /etc/passwd | tr : \\t
